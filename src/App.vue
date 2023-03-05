1744d52609e6acf007ea95af52b5dbf0a98e8b25
omersenem <omerrsenem@gmail.com>
2023-03-05 13:18:31 +0300

    appProduct complated

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,61 @@
 <template>
-  <h1 class="text-blue-600 ">bismillah</h1>
+  <div id="wrapper">
+    <div class="header ">
+      <h1 class="main-title text-blue-400 text-2xl">Alışveriş Sepeti Uygulaması - {{wallet}} $</h1>
+      <p1 class="purchased-count-text text-center">Şu ana kadar {{purchasedProduct.length}} tane ürün aldınız...</p1>
+
+    </div>
+   <div class="product-list">
+     <app-product-item
+         v-for="product in products" :key="product.id"
+         :product="product" @purchaseProductById="purchadeProductById"
+         :isPurchasedProduct="isPurchasedProduct"
+     ></app-product-item>
+   </div>
+    <pre>
+      {{purchasedProduct}}
+    </pre>
+  </div>
+
 </template>
+
+<script setup>
+ import { products } from '@/constants/data'
+import AppProductItem from "@/components/appProductItem.vue";
+import {ref} from "vue";
+
+const wallet = ref(250.0)
+ const purchasedProduct =ref([])
+ const purchadeProductById = (product)=>{
+  purchasedProduct.value.push(product)
+ }
+
+ const isPurchasedProduct = (id) =>{
+  const product = purchasedProduct.value.find(
+      (purchasedProductItem) => purchasedProductItem.id == id
+  );
+  if(product){
+    return true;
+  } else{
+    return false;
+  }
+
+
+ }
+
+</script>
+
+<style scoped>
+body{
+  font-family: ".SF NS Mono",serif !important;
+  background-color: #f1f1f1;
+}
+#wrapper{
+  @apply
+  flex items-center justify-center flex-col mt-5
+}
+.product-list{
+  @apply
+  flex items-center justify-center gap-3 flex-wrap
+}
+</style>
---
+++ src/components/appProductItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="product  ">
+    <div class="left">
+      <img class="product-img" :src="props.product.image" alt="">
+      <div class="product-info">
+        <h1 class="product-title">
+          {{props.product.title}}
+
+        </h1>
+        <span class="product-price">${{props.product.price.toFixed(2)}}</span>
+      </div>
+    </div>
+    <button v-if="isPurchasedProduct(props.product.id) == false" @click="handleBuy" class="hover:bg-green-300 bg-green-500 p-1 rounded-lg cursor-pointer">Add fruit</button>
+  </div>
+
+</template>
+
+<script setup>
+
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  product: Object,
+  isPurchasedProduct: Function
+})
+const handleBuy = ()=>{
+  emit('purchaseProductById', props.product)
+}
+const emit = defineEmits(['purchaseProductById'])
+</script>
+
+<style scoped>
+.product{
+   @apply
+   max-w-[300px] w-full
+   mt-3 bg-blue-100  rounded-lg p-2
+   flex items-center  gap-3 justify-between
+}
+.left{
+  @apply
+  flex items-center gap-3 justify-center
+}
+.product-img{
+  @apply
+  w-12 h-12  rounded-full
+}
+.product-info{
+  @apply
+  flex flex-col gap-1.5
+}
+.product-title{
+  @apply
+  text-lg font-bold
+}
+.product-price{
+  @apply
+  text-sm font-normal
+}
+</style>
